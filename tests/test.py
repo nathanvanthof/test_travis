79424af12639a637518228b52688ae471f24d4a5
nathana97@hotmail.com <nathan.vanthof@dutchanalytics.com>
2018-12-07 10:11:00 +0100

    Testing loading json

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,7 @@
 import unittest
 from main import main
+import os
+import json
 
 
 class TestFunctions(unittest.TestCase):
@@ -9,6 +11,10 @@ class TestFunctions(unittest.TestCase):
 
     def test_fail(self):
         self.assertEqual(1, 5)
+
+    def test_secure(self):
+        data = json.loads(os.environ('example'))
+        self.assertEqual(data['a'], 1)
 
 
 if __name__ == '__main__':
