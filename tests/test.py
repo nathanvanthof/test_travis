3e42cc7d038738383d94e2f6f409da201949d044
nathana97@hotmail.com <nathan.vanthof@dutchanalytics.com>
2019-03-20 15:00:06 +0100

    Fixed failing test

--- tests/test.py
+++ tests/test.py
@@ -7,7 +7,7 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(0, 0)
 
     def test_fail(self):
-        self.assertEqual(1, 5)
+        self.assertNotEqual(1, 5)
 
 
 if __name__ == '__main__':
